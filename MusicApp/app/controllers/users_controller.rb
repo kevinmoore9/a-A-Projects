a24665524fd0de0bdb6de163505fe265fba2d2ba
Kevin <kevinmoore9@users.noreply.github.com>
2017-04-24 16:30:40 -0700

    user controller

--- MusicApp/app/controllers/users_controller.rb
+++ MusicApp/app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in!(@user)
+      redirect_to bands_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+
+    def show
+      @user = User.find_by(id: params[:id])
+      render :show
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
